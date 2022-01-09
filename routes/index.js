5b3c27f5fb62e7b0dfac3255a022399ad6e753d1
Happy Kumamoto <zen.word+hub@gmail.com>
2022-01-10 00:27:07 +0900

    login and logout

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.user); //iconの情報を取得
   res.render('index', { title: 'Express' , user: req.user });
 });
 
